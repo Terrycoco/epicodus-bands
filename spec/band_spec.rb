19d064e9d5335cf12abf559ac3b0581fd14c20ba
Terry <terrymarr280@gmail.com>
2015-08-28 11:40:26 -0700

    ensure venues are not duplicated

--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -8,19 +8,15 @@ describe (Band) do
       test_venue = Venue.create({:venue_name => 'The Roxy'})
       test_band.gigs << Gig.new({:venue => test_venue, :gig_date => '1999-01-01 14:30'})
       expect(test_band.venues).to(eq([test_venue]))
-
     end
-
   end
 
   describe('tests validation for uniqueness ') do
     it('creates a band and ensures no duplication') do
       test_band1 = Band.create({:band_name => 'Aerosmith'})
       test_band2 = Band.create({:band_name => 'Aerosmith'})
-      expect(Band.all).to(eq([test_band1]))
-
+      expect(Band.all.count()).to(eq(1))
     end
-
   end
 
 
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -8,11 +8,17 @@ describe (Venue) do
       test_venue = Venue.create({:venue_name => 'The Roxy'})
       test_venue.gigs << Gig.new({:band => test_band, :gig_date => '1999-01-01 14:30'})
       expect(test_venue.bands).to(eq([test_band]))
-
     end
-
   end
 
+
+    describe('tests validation for uniqueness ') do
+      it('creates a vanue and ensures no duplication') do
+        testv1 = Venue.create({:venue_name => 'The Roxy'})
+        testv1 = Venue.create({:venue_name => 'The Roxy'})
+        expect(Venue.all.count()).to(eq(1))
+      end
+    end
 
 
 end
