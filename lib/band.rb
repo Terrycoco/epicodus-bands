9785dc0dfd2db98788e4ddfc57c061c11c1d29ca
Terry <terrymarr280@gmail.com>
2015-08-28 14:45:17 -0700

    adds capitalization for venue and band names

--- lib/band.rb
+++ lib/band.rb
@@ -2,4 +2,10 @@ class Band < ActiveRecord::Base
   has_many :venues, :through => :gigs
   has_many :gigs
   validates :band_name, presence: true, uniqueness: true
+  before_save :capitalize
+
+  private
+  define_method (:capitalize) do
+    self.band_name = self.band_name.capitalize
+  end
 end
--- lib/venue.rb
+++ lib/venue.rb
@@ -2,4 +2,10 @@ class Venue < ActiveRecord::Base
   has_many :bands, :through => :gigs
   has_many :gigs
   validates :venue_name, uniqueness: true
+  before_save :capitalize
+
+  private
+  define_method (:capitalize) do
+    self.venue_name = self.venue_name.capitalize
+  end
 end
