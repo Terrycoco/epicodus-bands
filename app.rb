a5a7a36856d893c6fc56f1463d251e0f7eb8b615
Terry <terrymarr280@gmail.com>
2015-08-28 13:33:27 -0700

    display lists of bands and venues, with edit pages for each

--- app.rb
+++ app.rb
@@ -2,3 +2,55 @@ require 'bundler/setup'
 Bundler.require :default
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+#bands
+get('/') do
+  @bands = Band.all
+  erb(:bands)
+end
+
+post('/bands/new') do
+  Band.create({:band_name => params.fetch("band_name")})
+  redirect('/')
+end
+
+get('/bands/:bid/edit') do
+  @band = Band.find(params.fetch('bid'))
+  @venues = Venue.all
+  erb(:band_edit)
+end
+
+post('/bands/:bid/gig/add') do
+  bid = params.fetch('bid')
+  band = Band.find(bid)
+  venue = Venue.find(params.fetch('venue_id'))
+  gig_date = params.fetch("gig_date")
+  band.gigs << Gig.new({:venue => venue, :gig_date => gig_date})
+  redirect("/bands/#{bid}/edit")
+end
+
+#venues
+get('/venues') do
+  @venues = Venue.all
+  erb(:venues)
+end
+
+post('/venues/new') do
+  Venue.create({:venue_name => params.fetch("venue_name")})
+  redirect('/venues')
+end
+
+get('/venues/:vid/edit') do
+  @venue = Venue.find(params.fetch('vid'))
+  @bands = Band.all
+  erb(:venue_edit)
+end
+
+post('/venues/:vid/gig/add') do
+  vid = params.fetch('vid')
+  venue = Venue.find(vid)
+  band = Band.find(params.fetch('band_id'))
+  gig_date = params.fetch("gig_date")
+  band.gigs << Gig.new({:venue => venue, :gig_date => gig_date})
+  redirect("/venues/#{vid}/edit")
+end
